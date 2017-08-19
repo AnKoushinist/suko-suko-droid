d1037a01d937a5da5cabf84a93925ada7dbd75f7
AnKoushinist <109f713c@sigaint.org>
2017-08-20 01:38:39 +0900

    only appearance work

--- app/src/main/java/ankoushinist/SukoSukoDroid/MainActivity.java
+++ app/src/main/java/ankoushinist/SukoSukoDroid/MainActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,15 +39,48 @@ public class MainActivity extends AppCompatActivity {
             password=findViewById(R.id.password);
             channel=findViewById(R.id.channelId);
             start=findViewById(R.id.start);
+
+            start.setOnClickListener(a -> {
+                SukoSukoWorkerFragment worker = new SukoSukoWorkerFragment();
+                Bundle command = new Bundle();
+                command.putString("email", email.getText().toString());
+                command.putString("password", password.getText().toString());
+                command.putString("channel", channel.getText().toString());
+                worker.setArguments(command);
+                getActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, worker)
+                    .commit();
+            });
+        }
+    }
+    public static class SukoSukoWorkerFragment extends FuckingFragment{
+        WebView wv;
+        LinearLayout logContainer;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.activity_sukosuko,container,false);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            wv=findViewById(R.id.sukosukoDisplay);
+            logContainer=findViewById(R.id.sukosukoTextViewContainer);
+
         }
     }
 
 
 
 
+
     public static abstract class FuckingFragment extends Fragment{
         public <T extends View> T findViewById(@IdRes int id){
-            return getView().findViewById(id);
+            return (T)getView().findViewById(id);
         }
     }
 }
